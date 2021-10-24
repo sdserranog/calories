7c0ce8df8574f8fa7e16fc1956c3e876c76e03ac
Sergio Serrano <sdserranog@gmail.com>
2021-10-23 23:27:11 -0400

    Add material ui

---
+++ src/config/theme/index.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  primary: {
+    main: '#24b47f',
+  },
+})
+
+export default theme
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom'
 import '@styles/index.css'
 import App from './App'
 
+import theme from '@config/theme'
+import { ThemeProvider } from '@mui/material/styles'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
