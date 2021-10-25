584eb584f160ca618c7a1321132d5900c786b5ee
Sergio Serrano <sdserranog@gmail.com>
2021-10-25 08:59:10 -0400

    Add home user

---
+++ src/components/Food/Modal.jsx
@@ -0,0 +1,104 @@
+import { useState } from 'react'
+
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal'
+import DateTimePicker from '@components/DateTimePicker'
+import useAuth from '@hooks/useAuth'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #ccc',
+  borderRadius: 6,
+  boxShadow: 16,
+  p: 4,
+}
+
+const FoodModal = ({ open, handleClose, onChange }) => {
+  const { user } = useAuth()
+
+  const [name, setName] = useState('')
+  const [calories, setCalories] = useState()
+  const [price, setPrice] = useState()
+  const [date, setDate] = useState(new Date())
+
+  const handleName = (e) => {
+    const { value } = e.target
+    setName(value)
+  }
+  const handleCalories = (e) => {
+    const { value } = e.target
+    setCalories(value)
+  }
+  const handlePrice = (e) => {
+    const { value } = e.target
+    setPrice(value)
+  }
+  const handleDate = (date) => {
+    setDate(date)
+  }
+
+  const handleCreate = () => {
+    const user_id = user.id
+    const data = { name, calories, price, date, user_id }
+    onChange(data)
+  }
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Food Entries
+        </Typography>
+        <TextField
+          sx={{ width: '100%', mt: 2 }}
+          id="product-name"
+          label="Product Name"
+          variant="outlined"
+          size="small"
+          value={name}
+          onChange={handleName}
+        />
+        <TextField
+          sx={{ width: '100%', mt: 2 }}
+          id="calories"
+          label="Calories"
+          variant="outlined"
+          size="small"
+          type="number"
+          value={calories}
+          onChange={handleCalories}
+        />
+        <TextField
+          sx={{ width: '100%', mt: 2 }}
+          id="price"
+          label="Price"
+          variant="outlined"
+          size="small"
+          type="number"
+          value={price}
+          onChange={handlePrice}
+        />
+        <Box sx={{ width: '100%', mt: 3 }}>
+          <DateTimePicker date={date} onChange={handleDate} />
+        </Box>
+        <Button
+          sx={{ width: '100%', mt: 3 }}
+          variant="contained"
+          onClick={handleCreate}
+        >
+          Create
+        </Button>
+      </Box>
+    </Modal>
+  )
+}
+
+export default FoodModal
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,56 @@
+import { useEffect, useState } from 'react'
+
+import Table from '@components/Table'
+import supabase from '@config/supabase'
+import ModalFood from '@components/Food/Modal'
+import Button from '@mui/material/Button'
+import AddIcon from '@mui/icons-material/Add'
+
 function Home() {
-  return <div className="App">Test</div>
+  const [open, setOpen] = useState(false)
+  const [data, setData] = useState([])
+
+  const handleCreate = async ({ name, calories, price, date, user_id }) => {
+    console.log({ name, calories, price, date })
+    const { data, error } = await supabase
+      .from('Food')
+      .insert([{ name, calories, price, created_at: date, user_id }])
+      .single()
+      .then(({ data, error }) => {
+        console.log(data, error)
+        if (!error) {
+          setData((prevData) => [data, ...prevData])
+        }
+        setOpen(false)
+      })
+  }
+
+  useEffect(() => {
+    supabase
+      .from('Food')
+      .select('*')
+      .then(({ data }) => {
+        setData(data)
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <Button
+        variant="contained"
+        onClick={() => setOpen(true)}
+        startIcon={<AddIcon />}
+      >
+        Add Food
+      </Button>
+      <Table data={data} />
+      <ModalFood
+        open={open}
+        handleClose={() => setOpen(false)}
+        onChange={handleCreate}
+      />
+    </div>
+  )
 }
 
 export default Home
