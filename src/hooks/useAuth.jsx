47d771dd9712410716379d8a63033695215576db
Sergio Serrano <sdserranog@gmail.com>
2021-10-25 08:58:37 -0400

    Add auth hook

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import supabase from '@config/supabase'
+
+function useAuth() {
+  const [user, setUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      const user = supabase.auth.user()
+      setUser(user)
+      setLoading(false)
+    }, 500)
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      async (event, user) => {
+        setUser(user)
+        setLoading(false)
+      }
+    )
+
+    return () => {
+      listener?.unsubscribe()
+    }
+  }, [])
+
+  const logout = () => supabase.auth.signOut()
+
+  return { user, loading, logout }
+}
+
+export default useAuth
